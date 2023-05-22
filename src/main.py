b0f770e2e78a21a6627c90d530a0c074c5fc079e
Anjul Sharma <1264595+anjul@users.noreply.github.com>
2023-05-22 08:12:19 +0800

    added files to test cicd

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+class DisplayDate:
+
+    def __init__(self) -> None:
+        pass
+
+    def display_current_timestamp(self):
+        now = datetime.now()
+        print(f'Current Date and Time is {now.strftime("%Y-%m-%d %H:%M:%S")}')
+
+DisplayDate().display_current_timestamp()
---
+++ tests/src/test_main.py
@@ -0,0 +1,4 @@
+def test_main():
+    str_a = 'Sample String'
+    str_b = 'Sample String'
+    assert str_a == str_b
